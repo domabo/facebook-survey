fa9897b8f26b6d04806a93290c4aacdfbe6bf191
domabo.com <admin@domabo.com>
2014-06-17 22:41:00 -0500

    Update facebook-survey.php

--- facebook-survey.php
+++ facebook-survey.php
@@ -101,9 +101,9 @@ function statusChangeCallback(response) {
 
 <div id='registerFB' style='visibility: hidden;'>
   <iframe src='https://www.facebook.com/plugins/registration?client_id=" . get_option('FACEBOOK_APP_ID'). ">&amp;redirect_uri=". plugins_url( 'facebook-survey-continue.php',  __FILE__ ) . "?success=". $a['success']." &amp;fb_only=true&amp;fields=name,first_name,last_name,email' 
-  width='450' height='330'>
+  width='450' height='340'>
   </iframe>
-  <h6>Not you?<div class='fb-login-button' scope='public_profile,email' data-max-rows='1' data-size='large' data-show-faces='false' data-auto-logout-link='true' onlogin='checkLoginState();'></div></h6>
+  <h6>Not you?<div class='fb-login-button' scope='public_profile,email' data-max-rows='1' data-size='small' data-show-faces='false' data-auto-logout-link='true' onlogin='checkLoginState();'></div></h6>
 </div>";
         }
 
