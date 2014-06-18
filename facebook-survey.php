71931a979565487faf9f2063eb0e158a745ca28c
domabo.com <admin@domabo.com>
2014-06-17 20:57:41 -0500

    Update facebook-survey.php

--- facebook-survey.php
+++ facebook-survey.php
@@ -50,13 +50,43 @@ class fsm_Plugin {
 		
         	return 
         	"<div id='fb-root'></div>
-<script>(function(d, s, id) {
+<script>// <![CDATA[
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=507296919397910&version=v2.0';
+  js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=" . get_option('FACEBOOK_APP_ID')."&version=v2.0';
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+}(document, 'script', 'facebook-jssdk'));
+
+		window.fbAsyncInit = function() { 
+			FB.init({
+      				appId  : '" . get_option('FACEBOOK_APP_ID') . "',
+      				status : true, // check login status
+      				cookie : false, // enable cookies to allow the server to access the session
+      				xfbml  : false  // parse XFBML
+    				});
+			FB.getLoginStatus(function(o) { 
+       			if (o.status == 'connected') {
+          			// USER IS LOGGED IN AND HAS AUTHORIZED APP
+         			document.getElementById('registerFB').style.display='block';
+         			 document.getElementById('loginFB').style.display='block';
+       			} else if (o.status == 'not_authorized') {
+          			// USER IS LOGGED IN TO FACEBOOK (BUT HASN'T AUTHORIZED YOUR APP YET)
+          			document.getElementById('registerFB').style.display='block';
+          			document.getElementById('loginFB').style.display='block';
+       			} else {
+          			 document.getElementById('registerFB').style.display='none';
+          			 document.getElementById('loginFB').style.display='block';
+       			}
+    			});};
+			// ]]></script>
+		
+			<div id='registerFB' style='display: none;'>
+			<h2>First we need to confirm your identity using Facebook</h2>
+			<iframe src='https://www.facebook.com/plugins/registration?client_id=" . get_option('FACEBOOK_APP_ID'). ">&amp;redirect_uri=". plugins_url( 'facebook-survey-continue.php',  __FILE__ ) . "?success=". $a['success']." &amp;fb_only=true&amp;fields=name,first_name,last_name,email' width='450' height='450'>
+			</iframe>
+			</div>
         
 			<div id='loginFB' style='display: block;'>
 			<h2>First we need you to confirm your identity using Facebook</h2>
