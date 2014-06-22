612b6e265c04c1c3adaec66fa7c26c453ce1235a
Domabo <admin@domabo.com>
2014-06-22 12:43:34 -0500

    updated podio

--- facebook-survey.php
+++ facebook-survey.php
@@ -9,6 +9,26 @@ Author URI: http://www.github.com/domabo
 License: GPLv2 or later
 */
 
+  require_once( 'Facebook/FacebookSession.php' );
+  require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
+  require_once( 'Facebook/FacebookRequest.php' );
+  require_once( 'Facebook/FacebookResponse.php' );
+  require_once( 'Facebook/FacebookSDKException.php' );
+  require_once( 'Facebook/FacebookRequestException.php' );
+  require_once( 'Facebook/FacebookAuthorizationException.php' );
+  require_once( 'Facebook/GraphObject.php' );
+  require_once( 'Facebook/HttpClients/FacebookCurl.php' );
+  require_once( 'Facebook/HttpClients/FacebookHttpable.php' );
+  require_once( 'Facebook/HttpClients/FacebookCurlHttpClient.php' );
+
+  use Facebook\FacebookSession;
+  use Facebook\FacebookRedirectLoginHelper;
+  use Facebook\FacebookRequest;
+  use Facebook\FacebookResponse;
+  use Facebook\FacebookSDKException;
+  use Facebook\FacebookRequestException;
+  use Facebook\FacebookAuthorizationException;
+  use Facebook\GraphObject;
 
 
 class fsm_Plugin {
@@ -117,26 +137,6 @@ public function fsm_shortcode_session_verify( $atts ){
 //[facebook-survey]
 public function fsm_shortcode_facebook_survey( $atts ){
 
-  require_once( 'Facebook/FacebookSession.php' );
-  require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
-  require_once( 'Facebook/FacebookRequest.php' );
-  require_once( 'Facebook/FacebookResponse.php' );
-  require_once( 'Facebook/FacebookSDKException.php' );
-  require_once( 'Facebook/FacebookRequestException.php' );
-  require_once( 'Facebook/FacebookAuthorizationException.php' );
-  require_once( 'Facebook/GraphObject.php' );
-  require_once( 'Facebook/HttpClients/FacebookCurl.php' );
-  require_once( 'Facebook/HttpClients/FacebookHttpable.php' );
-  require_once( 'Facebook/HttpClients/FacebookCurlHttpClient.php' );
-
-  use Facebook\FacebookSession;
-  use Facebook\FacebookRedirectLoginHelper;
-  use Facebook\FacebookRequest;
-  use Facebook\FacebookResponse;
-  use Facebook\FacebookSDKException;
-  use Facebook\FacebookRequestException;
-  use Facebook\FacebookAuthorizationException;
-  use Facebook\GraphObject;
 
 
   $a = shortcode_atts( array(
