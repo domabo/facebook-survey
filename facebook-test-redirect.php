41ae4693eea79be6291d358158ff81895ed69d3b
Domabo <admin@domabo.com>
2014-06-22 12:06:50 -0500

    updated podio

--- facebook-test-redirect.php
+++ facebook-test-redirect.php
@@ -54,9 +54,8 @@ if ($session) {
 			$session, 'GET', '/me'
 			))->execute()->getGraphObject(GraphUser::className());
 
-		print_r($user);
 
-echo "HELLO " . $user->first_name;
+echo $user->first_name;
 	
 
 	} catch(FacebookRequestException $e) {
